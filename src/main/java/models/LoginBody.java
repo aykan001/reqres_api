c6d41096ff95250ee3994a948494637d2ddf5a5c
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-04-27 17:19:38 +0300

    LoginBody model updated

--- src/main/java/models/LoginBody.java
+++ src/main/java/models/LoginBody.java
@@ -1,12 +1,23 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class LoginBody {
+    @JsonProperty("email")
     private String email;
+    @JsonProperty("password")
     private String password;
 
     public LoginBody(String email, String password) {
         this.email = email;
         this.password = password;
     }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
 
 }
