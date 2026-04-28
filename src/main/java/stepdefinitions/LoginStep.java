143d38d653628d9ae71fc66c0bccb90bcfe8c0d5
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-04-28 18:29:40 +0300

    Created LoginStep.java

---
+++ src/main/java/stepdefinitions/LoginStep.java
@@ -0,0 +1,42 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+import models.LoginBody;
+import servers.LoginServer;
+import servers.RegisterServer;
+import utils.TextContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginStep {
+    TextContext context;
+
+    public LoginStep(TextContext context) {
+        this.context = context;
+    }
+
+    @Given("login icin {string} ve {string} body gönder")
+    public void setLoginBody(String email, String password) {
+        if(email.equals("NULL") || password.equals("NULL") ||
+                email.equals("EMPTY") || password.equals("EMPTY")) {
+
+            Map<String, Object> body = new HashMap<>();
+
+            if (!email.equals("NULL")) {
+                body.put("email", email.equals("EMPTY") ? "" : email);
+            }
+
+            if (!password.equals("NULL")) {
+                body.put("password", password.equals("EMPTY") ? "" : password);
+            }
+
+            context.response = RegisterServer.postRegister(body);
+        }else {
+            LoginBody body=new LoginBody(email, password);
+            context.response= LoginServer.postLogin(body);
+        }
+
+
+    }
+}
