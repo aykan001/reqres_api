9f28a19210eb26a9518199ad169c2f77474fb93b
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-04-30 22:23:32 +0300

    Updated UpdateBody.java

--- src/main/java/models/UpdateBody.java
+++ src/main/java/models/UpdateBody.java
@@ -1,4 +1,20 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UpdateBody {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("job")
+    private String job;
+    public UpdateBody(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getJob() {
+        return job;
+    }
 }
