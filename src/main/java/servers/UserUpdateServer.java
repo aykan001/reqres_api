2abf298c536616b99c98972fd9099b8ed44eca23
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-04-30 22:18:15 +0300

    UserUpdateServer updated

--- src/main/java/servers/UserUpdateServer.java
+++ src/main/java/servers/UserUpdateServer.java
@@ -3,10 +3,11 @@ package servers;
 import endpoints.Api_Endpoints;
 import io.restassured.response.Response;
 
-public class UpdateUserServer {
-
-    public Response getUserUpdate(Object body){
+public class UserUpdateServer {
+    public static Response getUserUpdate(Object body){
         return BaseServer.putRequest(Api_Endpoints.USER_UPDATE_ENDPOINT,body);
     }
-
+    public static Response getUserUpdateInvalid(String rawBody){
+        return BaseServer.putUserUpdateRaw(Api_Endpoints.USER_UPDATE_ENDPOINT,rawBody);
+    }
 }
