a3420769c9c1c564929c16807f41c8508f8777d6
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-04-30 22:20:45 +0300

    RepeatStep update

--- src/main/java/stepdefinitions/RepeatStep.java
+++ src/main/java/stepdefinitions/RepeatStep.java
@@ -11,7 +11,7 @@ public class RepeatStep {
     public RepeatStep(TextContext context){this.context=context;}
 
     @Given("wrong {string} request atilir")
-    public void getWrongRequestAtilir(String endpoint) {
+    public void getWrongEndpointRequestAtilir(String endpoint) {
         context.response= RepeatServer.getWrongEndpoint(endpoint);
     }
     @Given("without auth {string} request atilir")
@@ -22,6 +22,7 @@ public class RepeatStep {
     public void getNoEmptyResponse(){
         assertNotNull(context.response.jsonPath().get("data"));
     }
+
     @And("response icin token olmali")
     public void responseIcinTokenOlmali() {
         String token=context.response.jsonPath().get("token");
