97db1ee3b80c606c5620c4afd74aba57c44a1913
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-05 17:56:40 +0300

    updated

--- src/main/java/stepdefinitions/UserStep.java
+++ src/main/java/stepdefinitions/UserStep.java
@@ -2,7 +2,12 @@ package stepdefinitions;
 
 import io.cucumber.java.en.*;
 import client.UserClient;
+import models.UpdateBody;
+import utils.BodyBuilder;
 import utils.TextContext;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import static org.junit.Assert.*;
 
 public class UserStep {
 
@@ -19,5 +24,44 @@ public class UserStep {
     public void userByUrl() {
         new UserClient();
         context.response = UserClient.getUser("2");
+    }
+    @Given("users list request atilir")
+    public void getUsers() {
+        context.response = UserClient.getUsers();
+    }
+
+    @Given("user update icin {string} ve {string} body gonder")
+    public void updateUser(String name, String job) {
+        context.response = UserClient.updateUser(
+                BodyBuilder.user(name, job)
+        );
+    }
+    @Given("user update icin {string} ve {string} valid body gonder")
+    public void updateInvalidBodyUser(String name, String job) {
+        new UserClient();
+        context.response = UserClient
+                .updateUser(BodyBuilder.user(name, job));
+    }
+    @And("{string} {string} bos olmamali ve dogru olmali")
+    public void fieldCheck(String field, String expected) {
+        String actual = context.response.jsonPath().get(field);
+        assertEquals(expected, actual);
+    }
+    @And("updatedAt bos olmamali ve dogru olmali")
+    public void updatedAtCheck() {
+        String updatedAt = context.response.jsonPath().get("updatedAt");
+
+        assertNotNull(updatedAt);
+
+        OffsetDateTime time = OffsetDateTime.parse(updatedAt);
+        OffsetDateTime now = OffsetDateTime.now();
+
+        assertTrue(Duration.between(time, now).getSeconds() < 5);
+    }
+    @Given("user updated icin without auth {string} {string} body gonder")
+    public void userUpdatedIcinWithoutAuthBodyGonder(String name, String job) {
+        new UserClient();
+        UpdateBody body=new UpdateBody(name,job);
+        context.response=UserClient.updateUserNoAuth(body);
     }
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -14,6 +14,13 @@ Feature:User and Users API Test
     Then response status code 200 olmali
     And response bos olmamali
 
+  Scenario: Successful update
+    Given user update icin "morpheus" ve "zion resident" valid body gonder
+    Then response status code 200 olmali
+    And "name" "morpheus" bos olmamali ve dogru olmali
+    And "job" "zion resident" bos olmamali ve dogru olmali
+    And updatedAt bos olmamali ve dogru olmali
+
   Scenario Outline: Invalid endpoint
     ****** 404 fırlatıyor beklenen ama Then görmeden fail veriyor*****
     Given wrong "<endpoint>" request atilir
@@ -22,7 +29,6 @@ Feature:User and Users API Test
       | endpoint |
       | /users/a |
       | //users/ |
-
 
   Scenario Outline:Case-N01 Wrong endpoint cases
   ****404 vermesi gereken yerde 200 veriyor beklenen dışı davranıyor *****
@@ -37,4 +43,21 @@ Feature:User and Users API Test
   ****401 vermesi gereken yerde 200 dönüyor *****
     Given without auth "/users" request atilir
     Then response status code 200 olmali
+
+  Scenario Outline: Invalid update data
+  ****400 donmesi gereke ama 200 donuyor beklenen dişi****
+    Given user update icin "<name>" ve "<job>" valid body gonder
+    Then response status code 200 olmali
+    Examples:
+      | name  | job   |
+      | NULL  | NULL  |
+      | EMPTY | EMPTY |
+
+  Scenario Outline: Without Auth updated user
+  ****401 alıyoruz doğru ama statusCode gelmeden hata fırlatıyor*********
+    Given user updated icin without auth "<name>" "<job>" body gonder
+    Then response status code 401 olmali
+    Examples:
+      | name     | job           |
+      | morpheus | zion resident |
 
