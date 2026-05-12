349ffb9c968d623e2031fa3e3d40620f755fdd28
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-12 11:18:34 +0300

    Update RecordyBody.java

--- src/main/java/models/RecordBody.java
+++ src/main/java/models/RecordBody.java
@@ -3,19 +3,17 @@ package models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RecordBody {
-    @JsonProperty("data")
-    private String data;
-    @JsonProperty("price")
-    private int price;
-    @JsonProperty("category")
-    private  String category;
-    @JsonProperty("in_stock")
-    private boolean in_stock;
 
-    public RecordBody(String data, boolean in_stock, int price, String category) {
+    @JsonProperty("data")
+    private RecordData data;
+
+    public RecordBody(RecordData data){
+        this.data=data;
+    }
+    public RecordData getData() {
+        return data;
+    }
+    public void setData(RecordData data) {
         this.data = data;
-        this.in_stock = in_stock;
-        this.price = price;
-        this.category = category;
     }
 }
