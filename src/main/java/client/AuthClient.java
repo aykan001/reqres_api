05cc85c4f7c4f22adb501012fe5ee0593f87d20f
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-12 11:11:53 +0300

    Update AuthClient.java
    The code was written in a simpler and more readable way.

--- src/main/java/client/AuthClient.java
+++ src/main/java/client/AuthClient.java
@@ -1,17 +1,20 @@
 package client;
 
 import io.restassured.response.Response;
+
 import static endpoints.Api_Endpoints.*;
 
 public class AuthClient extends BaseClient {
 
     public static Response login(Object body) {
-        return BaseClient.postRequest(LOGIN_ENDPOINT,body);
+        return BaseClient.sendUserRequest("POST", LOGIN_ENDPOINT, body);
     }
+
     public static Response register(Object body) {
-        return BaseClient.postRequest(REGISTER_ENDPOINT,body);
+        return BaseClient.sendUserRequest("POST", REGISTER_ENDPOINT, body);
     }
+
     public static Response loginWithoutApiKey(Object body) {
-        return BaseClient.postWithoutAuthRequest(LOGIN_ENDPOINT,body);
+        return BaseClient.sendNoAuthRequest("POST", LOGIN_ENDPOINT, body);
     }
 }
