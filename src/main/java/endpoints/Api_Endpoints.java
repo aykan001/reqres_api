10e236c1ef1d19bb10bb2b37f3d3f4eba16fcf1d
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-12 11:18:17 +0300

    Update Api_Endpoint.java

--- src/main/java/endpoints/Api_Endpoints.java
+++ src/main/java/endpoints/Api_Endpoints.java
@@ -1,7 +1,9 @@
 package endpoints;
 
 public class Api_Endpoints {
-    public static final String API_KEY = "reqres_937c25b68e884949afe910470c0550d9";
+    public static final String USER_API_KEY = "reqres_937c25b68e884949afe910470c0550d9";
+    public static final String RECORD_API_KEY="pro_9e71d88f7ab85ad0583772cb39018567eebc53db34d3ea4627327808b48e8ac0";
+    public static final String RECORD_X_REQRES_ENV="prod";
     public static final String BASE_URL = "https://reqres.in/api";
 
     public static final String USERS_LIST_ENDPOINT = "/users";
@@ -15,6 +17,10 @@ public class Api_Endpoints {
 
     public static final String USER_UPDATE_ENDPOINT = "/users/2";
 
-    public static final String LIST_RECORDS_ENDPOINT="/collections/products/records?project_id=14226";
-    public static final String RECORD_ENDPOINT="/collections/products/records/a6fea49d-69ec-473e-9994-c86dcdf5f40d?project_id=14226";
+
+    public static final String RECORD_ENDPOINT = "/users";
+    public static final String LIST_RECORDS_ENDPOINT = "/users";
+    public static final String CREATE_RECORD_ENDPOINT = "/users";
+
+
 }
