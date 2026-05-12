a7bfb3767ed7916d88688a7f4db9acc4072312bd
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-12 11:19:50 +0300

    Update BodyBuilder.java

--- src/main/java/utils/BodyBuilder.java
+++ src/main/java/utils/BodyBuilder.java
@@ -1,11 +1,25 @@
 package utils;
 
+import models.RecordBody;
+import models.RecordData;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class BodyBuilder {
+    public static RecordBody recordBodyBuilder(String name,
+                                               String price,
+                                               String category,
+                                               String in_stock){
+        RecordData recordData=new RecordData(
+                name,
+                Double.parseDouble(price),
+                category,
+                Boolean.parseBoolean(in_stock)
+        );
+        return new RecordBody(recordData);
+    }
 
-    // LOGIN + REGISTER
     public static Map<String, Object> auth(String email, String password) {
 
         Map<String, Object> body = new HashMap<>();
@@ -20,8 +34,6 @@ public class BodyBuilder {
 
         return body;
     }
-
-    // USER UPDATE
     public static Map<String, Object> user(String name, String job) {
 
         Map<String, Object> body = new HashMap<>();
@@ -34,6 +46,24 @@ public class BodyBuilder {
             body.put("job", "EMPTY".equals(job) ? "" : job);
         }
 
+        return body;
+    }
+    public static Map<String,Object> record(String name, boolean in_stock, double price, String category){
+        Map<String,Object> body=new HashMap<>();
+
+        if (!"NULL".equals(name)) {
+            body.put("name", "EMPTY".equals(name) ? "" : name);
+        }
+        if (!"NULL".equals(in_stock)) {
+            body.put("in_stock", "EMPTY".equals(in_stock) ? "" : in_stock);
+        }
+        if (!"NULL".equals(price)) {
+            body.put("price", "EMPTY".equals(price) ? "" : price);
+        }
+
+        if (!"NULL".equals(category)) {
+            body.put("category", "EMPTY".equals(category) ? "" : category);
+        }
         return body;
     }
 }
