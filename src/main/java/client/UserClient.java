6bd62438eea8ccf603b9ef6139c1e0a3ce3c851f
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2026-05-12 11:17:47 +0300

    Update UserClient.java
    The code was written in a simpler and more readable way.

--- src/main/java/client/UserClient.java
+++ src/main/java/client/UserClient.java
@@ -4,19 +4,24 @@ import io.restassured.response.Response;
 import static endpoints.Api_Endpoints.*;
 
 public class UserClient extends BaseClient {
+
     public static Response getUser(String id) {
-        return BaseClient.getRequest(USER_ENDPOINT+id);
+        return sendUserRequest("GET", USER_ENDPOINT + id, null);
     }
+
     public static Response getUsers() {
-        return BaseClient.getRequest(USERS_LIST_ENDPOINT);
+        return sendUserRequest("GET", USERS_LIST_ENDPOINT, null);
     }
+
     public static Response updateUser(Object body) {
-        return BaseClient.putRequest(USER_UPDATE_ENDPOINT,body);
+        return sendUserRequest("PUT", USER_UPDATE_ENDPOINT, body);
     }
+
     public static Response updateUserNoAuth(Object body) {
-        return BaseClient.putWithoutAuthRequest(USER_UPDATE_ENDPOINT,body);
+        return sendNoAuthRequest("PUT", USER_UPDATE_ENDPOINT, body);
     }
-    public static Response deleteUser(String id){
-        return BaseClient.deleteRequest(USER_ENDPOINT+id);
+
+    public static Response deleteUser(String id) {
+        return sendUserRequest("DELETE", USER_ENDPOINT + id, null);
     }
 }
